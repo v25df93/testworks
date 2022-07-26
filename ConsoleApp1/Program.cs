250f40b3ca9202be882f7ce1b6840f51c1e7ff6c
vladimir vekhov <badcater@gmail.com>
2022-07-26 13:40:47 +0300

    закоментировано

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,10 @@
-﻿public class Animal
+﻿public class Animal  //само животное
 {
     protected string name;
     protected int calories;
     protected int amount;
-    public int days;
 
-    public Animal()
+    public Animal()  //имеет имя калории и количество
     {
         this.name = "0";
         this.calories = 0;
@@ -13,7 +12,7 @@
 
     }
 
-    public Animal(string name, int calories, int amount)
+    public Animal(string name, int calories, int amount) //привязка переменных
     {
         this.name = name;
         this.calories = calories;
@@ -21,33 +20,27 @@
 
 
     }
-
-    public string getNameAnimal()
+    public string getNameAnimal() //возврат имени животного
     {
         return name;
     }
 
-    //public int getCaloriesPerMonth()
-    //{
-    //    return calories * amount * 30;
-
-    //}
-    public int getAmountAnimal()
+    public int getAmountAnimal() //возврат количества животного
     {
         return amount;
     }
-    public int getCaloriesAnimal()
+    public int getCaloriesAnimal() //возврат требуемых калорий животного
     {
         return calories;
     }
 
 };
-class Plant : Animal
+class Plant : Animal  //травоядное
 {
 
     float kgOfFood;
     int caloriesInBanana = 960;
-    public Plant(string name, int calories, int amount)
+    public Plant(string name, int calories, int amount) //учет килограм еды для травоядного
     {
         this.name = name;
         this.calories = calories;
@@ -55,16 +48,16 @@ class Plant : Animal
         kgOfFood = (float)amount * calories / caloriesInBanana;
     }
 
-    public float getBananas()
+    public float getBananas()  //возврат килограм еды для травоядного
     {
         return kgOfFood;
     }
 }
-class Meat : Animal
+class Meat : Animal        //мясоед
 {
     float kgOfFood;
     int caloriesInMeat = 3180;
-    public Meat(string name, int calories, int amount)
+    public Meat(string name, int calories, int amount)  //учет килограм еды для мясоеда
     {
         this.name = name;
         this.calories = calories;
@@ -73,19 +66,19 @@ class Meat : Animal
     }
 
 
-    public float getMeat()
+    public float getMeat()  //возврат килограм еды сколько нужно мясоеду
     {
         return kgOfFood;
     }
 }
-class meatPlant : Animal
+class meatPlant : Animal  //всеядные
 {
     private float kgOfMeat;
     private float kgOfBananas;
     private int caloriesInMeat = 3180;
     private int caloriesInBananas = 960;
 
-    public meatPlant(string name, int calories, int amount)
+    public meatPlant(string name, int calories, int amount)    //учет сколько килограммов еды нужно всеядному
     {
         this.name = name;
         this.calories = calories;
@@ -94,18 +87,18 @@ class meatPlant : Animal
         kgOfBananas = (float)amount * calories / (2 * caloriesInBananas);
     }
 
-    public float getMeat()
+    public float getMeat()  //возврат киломяса
     {
         return kgOfMeat;
     }
-    public float getBananas()
+    public float getBananas() //возврат килобананов
     {
         return kgOfBananas;
     }
 }
-class testanimal
+class testanimal  //класс вывода
 {
-    static int amountofdays()
+    static int amountofdays()   //ввод месяца
     {
         Console.WriteLine("Введите цифру месяца: \n 1)Январь \n 2)Февраль \n 3)Март \n 4)Апрель \n 5)Май \n 6)Июнь \n 7)Июль\n 8)Август \n 9)Сентябрь \n 10)Октябрь \n 11)Ноябрь \n 12)Декабрь");
         int month;
@@ -121,15 +114,15 @@ class testanimal
     }
 
 
-    static void Main()
+    static void Main()  //ввод количества животных и вывод данных
     {
         int amount;
         int days;
         float kgBanans = 0;
         float kgMeat = 0;
-        var theMeats = new List<Meat>();
-        var thePlants = new List<Plant>();
-        var theBoth = new List<meatPlant>();
+        var theMeats = new List<Meat>();        //мясоед
+        var thePlants = new List<Plant>();      //травоядное
+        var theBoth = new List<meatPlant>();    //всеядное
         days = amountofdays();
 
 
@@ -141,7 +134,7 @@ class testanimal
         amount = Convert.ToInt16(Console.ReadLine());
         theMeats.Add(new Meat("Лев", 10000, amount));
 
-        Console.WriteLine("Введите количество обезьян");
+        Console.WriteLine("Введите количество обезьян");     //добавление нового животного сюда
         amount = Convert.ToInt16(Console.ReadLine());
         thePlants.Add(new Plant("Обезьяна", 1000, amount));
 
@@ -161,14 +154,3 @@ class testanimal
         Console.WriteLine("Необходимо мяса: " + Math.Round(kgMeat * days, 1) + "кг " + "Необходимо бананов: " + Math.Round(kgBanans * days / 1) + "кг");
     }
 }
-
-
-
-
-// int amount = Convert.ToInt32(Console.ReadLine());
-
-//Animal bear = new Animal();
-
-//Animal bear("Медведь","Мясо");
-
-//Animal = Animal.Animal("bear", "monkey", "lion");
